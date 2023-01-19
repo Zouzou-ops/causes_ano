fad4da94fb7c11f683c77a1ede22d2ae3ae0a50d
Zouzou-ops <sofianezouaoui77@gmail.com>
2023-01-19 17:34:28 +0100

    Revert "first commit"
    This reverts commit 90e4001c818d60794613b76e067949231af7a96d.

--- bugs/healthcheck/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-class HealthCheckSerializer(serializers.Serializer):
-    requestedAt = serializers.DateTimeField()
-    message = serializers.CharField(max_length=255)
--- bugs/healthcheck/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('healthcheck/', views.healthcheck, name='healthcheck'),
-]
